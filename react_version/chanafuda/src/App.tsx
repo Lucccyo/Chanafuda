0a297f4ff61beb090fecab5c21153c831fe10e80
Lucccyo <cha.git@mailo.fr>
2024-02-13 15:33:07 +0100

    architecture and style of the front

--- react_version/chanafuda/src/App.tsx
+++ react_version/chanafuda/src/App.tsx
@@ -3,12 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import River from './River.tsx'
+import AllyHand from './AllyHand.tsx'
+import EnnemyHand from './EnnemyHand.tsx'
+import Stack from './Stack.tsx'
+import cards from './data.js'
+
+import { distribute } from './logic/game.js'
 
 function App() {
+  const [ally_hand, river, ennemy_hand, stack] = distribute(cards);
   return (
     <>
-      <p>Bonjour</p>
-      <River />
+      <div className="main">
+        <div className="board">
+          <div className="ennemy-hand">
+            <EnnemyHand cards={ennemy_hand} />
+          </div>
+          <div className="shared-board">
+            <div className="stack">
+              <Stack cards={stack}/>
+            </div>
+            <div className="river">
+              <River cards={river}/>
+            </div>
+          </div>
+          <div className="ally-hand">
+            <AllyHand cards={ally_hand} />
+          </div>
+        </div>
+        <div className="collection">
+          <div className="ennemy-collection">
+            <p>ENNEMY COLLECTION</p>
+          </div>
+          <div className="ally-collection">
+            <p>ALLY COLLECTION</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
