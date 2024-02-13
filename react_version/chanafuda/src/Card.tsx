4ad9aa1121ac00f535fdf369950adc3fc2fd7365
Lucccyo <cha.git@mailo.fr>
2024-02-13 15:30:12 +0100

    components showing or not cards

--- react_version/chanafuda/src/Card.tsx
+++ react_version/chanafuda/src/Card.tsx
@@ -2,8 +2,6 @@ const Card = ({info: { id, month, type, position }}) => {
   const card = {
       backgroundImage: "url('../hanafudraw_complete_edited.png')",
       backgroundPosition: position,
-      height: "64px",
-      width: "17px",
     };
   return (
     <div className="card" style={card}>
---
+++ react_version/chanafuda/src/ReversedCard.tsx
@@ -0,0 +1,11 @@
+const ReversedCard = () => {
+  const card = {
+      backgroundColor: "aqua",
+    };
+  return (
+    <div className="card" style={card}>
+    </div>
+  );
+}
+
+export default ReversedCard;
